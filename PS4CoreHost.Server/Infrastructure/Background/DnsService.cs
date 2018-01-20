b515a1093334bfedda77496a42f7b22cfcbbc492
Hugh Hoang <hugh.hoang@gmail.com>
2018-01-20 21:54:08 +1300

    Init commit

---
+++ PS4CoreHost.Server/Infrastructure/Background/DnsService.cs
@@ -0,0 +1,64 @@
+﻿using DNS.Client.RequestResolver;
+using DNS.Protocol;
+using DNS.Protocol.ResourceRecords;
+using DNS.Server;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PS4CoreHost.Server.Infrastructure.Background
+{
+
+    public class PSNetRequestResolver : IRequestResolver
+    {
+        public Task<IResponse> Resolve(IRequest request)
+        {
+            IResponse response = Response.FromRequest(request);
+
+            var question = request.Questions.FirstOrDefault(x => x.Name.ToString().Contains("playstation.net"));
+
+            if (question == null) return Task.FromResult<IResponse>(new Response());
+
+            IResourceRecord record = new IPAddressResourceRecord(question.Name, IPAddress.Parse("192.168.0.254"));
+            response.AnswerRecords.Add(record);
+            return Task.FromResult(response);
+        }
+    }
+
+    public class DnsHostedService : IHostedService, IDisposable
+    {
+        private DnsServer server = new DnsServer(new PSNetRequestResolver());
+
+        public void Dispose()
+        {
+            if (server != null)
+            {
+                server.Dispose();
+                server = null;
+            }
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            server.Requested += Server_Requested;
+            server.Listening += () => Console.WriteLine("DNS Listening...");
+
+            await server.Listen();
+        }
+
+        private void Server_Requested(IRequest request)
+        {
+            Console.WriteLine($"DNS -> {request.Questions.First().Name}");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            server.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PS4CoreHost.Server/Infrastructure/Middlewares/PS4ProxyMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS4CoreHost.Server.Infrastructure.Middlewares
+{
+    public class PS4ProxyMiddleware
+    {
+    }
+}
